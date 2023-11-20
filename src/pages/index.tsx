cc0069ae0fadfb2edce7aca5c761890659e1c227
Samuel Tobias Roland Uyet <samueltobiasrolanduyet@MacBook-Pro-tilhrende-Samuel.local>
2023-11-20 15:22:45 +0100

    color changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,19 @@
-import { Button } from "@/components/Button/Button";
+// import { Button } from "@/components/Button/Button";
 import { List } from "@/components/List/List";
 import { Modal } from "@/components/Modal/Modal";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
-      <Button label="okok" />
+      {/* <Button label="okok" /> */}
 
       <List item="test item" />
 
       <Modal title="Open modal" />
+
+      <Button variant="default">Button</Button>
     </>
   );
 }
