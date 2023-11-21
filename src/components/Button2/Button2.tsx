b2ea288f36e45ac77d15c3bb2d07578085517751
Tobias Roland <samu1493@stud.kea.dk>
2023-11-21 08:56:32 +0100

    components cleanup

--- src/components/Button2/Button2.tsx
+++
@@ -1,7 +0,0 @@
-interface Button2props {
-  label?: string;
-}
-
-export const Button2 = ({ label }: Button2props) => {
-  return <button>{label}</button>;
-};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,13 @@
 // import { Button } from "@/components/Button/Button";
-import { List } from "@/components/List/List";
-import { Modal } from "@/components/Modal/Modal";
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
-      {/* <Button label="okok" /> */}
 
-      <List item="test item" />
-
-      <Modal title="Open modal" />
-
-      <Button>Button</Button>
+      <Button className="">Button</Button>
     </>
   );
 }
