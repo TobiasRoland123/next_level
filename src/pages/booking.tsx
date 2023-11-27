d97b784ac670944d7a3170ba7b105546eefbc878
TheFred94 <frederikrlarsen@gmail.com>
2023-11-27 14:18:32 +0100

    Removed from booking

--- src/pages/booking.tsx
+++ src/pages/booking.tsx
@@ -1,10 +1,5 @@
 import { Hero } from "@/modules/Hero/Hero";
-import { DatePicker } from "@/components/Calender/DatePicker";
 
 export default function Booking() {
-  return (
-    <>
-      <DatePicker />
-    </>
-  );
+  return <></>;
 }
