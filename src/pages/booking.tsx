1d99a83e8358b825f50b6c6278585d1fdb52b894
TheFred94 <frederikrlarsen@gmail.com>
2023-11-27 14:17:55 +0100

    Datepicker works and can disable days

--- src/pages/booking.tsx
+++ src/pages/booking.tsx
@@ -1,5 +1,10 @@
 import { Hero } from "@/modules/Hero/Hero";
+import { DatePicker } from "@/components/Calender/DatePicker";
 
 export default function Booking() {
-  return <></>;
+  return (
+    <>
+      <DatePicker />
+    </>
+  );
 }
