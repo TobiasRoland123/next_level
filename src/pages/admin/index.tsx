c3d17d2c5aa5c2c5aa53112d17b3a321cb047af4
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2023-11-27 15:07:35 +0100

    Login works, might not be after the book thou'

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,9 @@
+export default function Admin() {
+  return (
+    <>
+      <main className="w-fit max-w-main pb-10 ">
+        <p> Hello! </p>
+      </main>
+    </>
+  );
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,49 @@
 import { FormEvent, useState } from "react";
 import { Input } from "@/components/Inputfields/Inputfield";
 import { Button } from "@/components/Button/Button";
+import { createClient } from "@supabase/supabase-js";
+import { useRouter } from "next/router";
 
 export default function Login() {
+  const supaUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supaKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+  const router = useRouter();
+  //   const supabase = createClientComponentClient();
   const [user, setUser] = useState({ email: "", password: "" });
-
-  const handleSubmit = (e: FormEvent) => {
+  const supabase = createClient("https://zwcshwxjwoffkdrdvbtp.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3Y3Nod3hqd29mZmtkcmR2YnRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEwNzg5NzgsImV4cCI6MjAxNjY1NDk3OH0.yq0erC0CIBZmUG9uMC8u1YVyG4g2dsf3PrpekxJDq34");
+  const handleLogin = (e: FormEvent) => {
     e.preventDefault();
-    console.log("Hello Jonas");
-    console.log(user);
+    console.log("user", user);
+    signInWithEmail();
   };
+
+  async function signInWithEmail() {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: user.email,
+      password: user.password,
+    });
+    if (data) {
+      router.push("/admin");
+    }
+  }
+
+  async function getSession() {
+    const { data, error } = await supabase.auth.getSession();
+    if (data.session === null) {
+      return;
+    }
+    router.push("/admin");
+  }
+
+  getSession();
   return (
     <>
-      <main className="max-w-screen-xl max-w-main w-full pb-10 ">
-        <section className=" bg-contrastCol/50 backdrop-blur-sm mt-28 mx-4 px-4 py-6 rounded-sm h-fit md:max-w-[66%]  lg:max-w-3xl  ">
-          <h3>
+      <main className="w-fit max-w-main pb-10 ">
+        <section className=" bg-contrastCol/50 backdrop-blur-sm mt-28 mx-4 px-4 py-6 rounded-sm h-fit  ">
+          <h3 className=" mb-4">
             Login to acess <span className="text-accentCol">admin page</span>
           </h3>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleLogin} className="w-full">
             <Input value={user.email} id="email" labelText="Email" type="email" onChange={(e) => setUser({ ...user, email: e.target.value })}></Input>
             <Input value={user.password} id="password" labelText="Password" type="password" className="mb-4" onChange={(e) => setUser({ ...user, password: e.target.value })}></Input>
             <Button>Login</Button>
