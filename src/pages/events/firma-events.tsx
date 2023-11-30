deacd512d1b7fe16b80e0fea4ab5f7d2d2644468
Tobias Roland <samu1493@stud.kea.dk>
2023-11-30 12:19:33 +0100

    basic site setup, missing responsiveness

--- src/pages/events/firma-events.tsx
+++ src/pages/events/firma-events.tsx
@@ -1,5 +1,99 @@
 import { Hero } from "@/modules/Hero/Hero";
+import Image from "next/image";
+import firmaEvent from "../../../public/images/event/firma-event.jpg";
 
 export default function FirmaEvents() {
-  return <></>;
+  return (
+    <>
+      <Hero
+        isFrontPage={false}
+        header="professionelle firma events"
+        redWord={["firma", "events"]}
+        content="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. "
+        buttonLabel="Kontakt os"
+      />
+      <section>
+        <article className="flex justify-center">
+          <div className="spacer w-full">
+            <h2>
+              Dit næste firma <span className="text-accentCol">event</span>
+            </h2>
+
+            <div className="flex flex-col gap-14">
+              <div>
+                <h3>Det perfekte sociale arrangement til kolleger</h3>
+                <p>
+                  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+                  literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+                  College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+                  going through the cites of the word in classical literature, discovered the undoubtable
+                </p>
+              </div>
+              <Image
+                src={firmaEvent}
+                width={1000}
+                height={1000}
+                alt="mænd der spiller computer"
+              />
+            </div>
+          </div>
+        </article>
+      </section>
+
+      <section>
+        <div className="flex justify-center">
+          <h2 className="spacer w-full">
+            Eksempler på tidligere <span className=" text-accentCol">Firma events</span>
+          </h2>
+        </div>
+        <article className="flex justify-center">
+          <div className="spacer flex flex-col w-full">
+            <div>
+              <h3>Eksempel 1</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor euismod posuere. Integer non ullamcorper
+                mi. Vestibulum et congue lorem. Suspendisse iaculis semper mauris eget aliquet. Mauris malesuada purus et rhoncus
+                auctor. Fusce vitae justo risus. Vestibulum convallis justo eu diam dignissim posuere. Donec ac maximus lectus.
+                Praesent non nibh eget sem tincidunt ultricies vel vitae magna. Nullam eget tellus id quam commodo iaculis
+                convallis a odio. Phasellus sagittis velit eget leo.
+              </p>
+            </div>
+            <Image
+              className="mt-6 md:mt-0"
+              src={firmaEvent}
+              width={1000}
+              height={1000}
+              alt="gutter der spiller"
+            />
+          </div>
+        </article>
+
+        <article className="flex justify-center">
+          <div className="spacer flex flex-col w-full">
+            <Image
+              className="order-2 mt-6 md:mt-0 md:order-1"
+              src={firmaEvent}
+              width={1000}
+              height={1000}
+              alt="gutter der spiller"
+            />
+            <div>
+              <h3>Eksempel 2</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor euismod posuere. Integer non ullamcorper
+                mi. Vestibulum et congue lorem. Suspendisse iaculis semper mauris eget aliquet. Mauris malesuada purus et rhoncus
+                auctor. Fusce vitae justo risus. Vestibulum convallis justo eu diam dignissim posuere. Donec ac maximus lectus.
+                Praesent non nibh eget sem tincidunt ultricies vel vitae magna. Nullam eget tellus id quam commodo iaculis
+                convallis a odio. Phasellus sagittis velit eget leo.
+              </p>
+            </div>
+          </div>
+        </article>
+      </section>
+
+      <section>
+        <div className="border-2 border-green-500"> small form comming soon</div>
+      </section>
+    </>
+  );
 }
