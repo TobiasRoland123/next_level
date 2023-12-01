d3bc1e8b2e26606b11ee63f5b27c796f3925e0a8
Tobias Roland <samu1493@stud.kea.dk>
2023-12-01 11:55:15 +0100

    made redword on priser into array of strings

--- src/pages/spil.tsx
+++ src/pages/spil.tsx
@@ -7,7 +7,7 @@ export default function Spil() {
       <Hero
         isFrontPage={false}
         header="Vores spil"
-        redWord="spil"
+        redWord={["spil"]}
         content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters"
       />
     </>
