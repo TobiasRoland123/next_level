3202dc2b8233cf8a5aedde77051c0670e833bec6
Tobias Roland <samu1493@stud.kea.dk>
2023-12-02 11:05:21 +0100

    added eventaCard variant

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -22,6 +22,7 @@ const cardVariants = cva("w-52 h-80 rounded bg-gradient-to-br cursor-pointer", {
       nlp: "text-amber-500 w-[938px] h-[507px] from-yellow-400 to-amber-600",
       bday1: "text-red-500 w-[343px] h-[460px] from-orange-500 to-red-600",
       bday2: "text-red-500 w-[343px] h-[460px] from-orange-500 to-red-600",
+      eventCard: "bg-contrastCol rounded-sm ",
     },
     size: {
       default: "px-1 py-1",
@@ -71,21 +72,20 @@ const Card = forwardRef<HTMLDivElement, CardProps>(
           tiltEnable={variant !== "nlp"}
           tiltMaxAngleX={5}
           tiltMaxAngleY={5}
-
-
           className="z-10"
-
         >
           <motion.div
             whileHover={
               variant !== "nlp"
-                ? {
-                    scale: [null, 1.25, 1.2],
-                    backgroundImage: `linear-gradient(var(--rotate), var(--${variant}Gradient))`,
-                    animation: "spin 1.7s linear infinite",
-                    zIndex: 50,
-                    boxShadow: `0px 0px 70px 30px var(--${variant}Shadow)`,
-                  }
+                ? variant !== "eventCard"
+                  ? {
+                      scale: [null, 1.25, 1.2],
+                      backgroundImage: `linear-gradient(var(--rotate), var(--${variant}Gradient))`,
+                      animation: "spin 1.7s linear infinite",
+                      zIndex: 50,
+                      boxShadow: `0px 0px 70px 30px var(--${variant}Shadow)`,
+                    }
+                  : {}
                 : {
                     scale: [null, 1.08, 1.05],
                     backgroundImage: `linear-gradient(var(--rotate), var(--${variant}Gradient))`,
@@ -112,22 +112,18 @@ const Card = forwardRef<HTMLDivElement, CardProps>(
                     variant === "bday1" || variant === "bday2" ? "h-[424px]" : "h-72"
                   } rounded`}
                 >
-
-                  <h2 className={clsx(variant, 'text-center text-4xl mt-0')}>{header}</h2>
-                  {variant === 'bday1' || variant === 'bday2' ? (
+                  <h2 className={clsx(variant, "text-center text-4xl mt-0")}>{header}</h2>
+                  {variant === "bday1" || variant === "bday2" ? (
                     <>
                       <h3 className=" text-secondaryCol text-center text-3xl mt-0">
-                        {variant === 'bday1' ? 'Standard' : 'Nlp'} pakke
+                        {variant === "bday1" ? "Standard" : "Nlp"} pakke
                       </h3>
                       <p className=" text-center text-secondaryCol uppercase mt-0">
-                        {variant === 'bday1' ? '(min. 8 personer)' : '(MAX. 10 personer)'}
+                        {variant === "bday1" ? "(min. 8 personer)" : "(MAX. 10 personer)"}
                       </p>
                     </>
                   ) : (
-                    <h3 className=" text-secondaryCol text-center text-3xl mt-0">
-                      {timeAntal} timer
-                    </h3>
-
+                    <h3 className=" text-secondaryCol text-center text-3xl mt-0">{timeAntal} timer</h3>
                   )}
 
                   <div className="h-[1px] my-3 bg-secondaryCol rounded"></div>
@@ -140,9 +136,7 @@ const Card = forwardRef<HTMLDivElement, CardProps>(
                             size="18"
                           />
                           <p className=" text-secondaryCol mt-0">
-
-                            3 timers gaming{' '}
-                            {variant === 'bday2' && '- I vores VIP rum med kraftigere computere'}
+                            3 timers gaming {variant === "bday2" && "- I vores VIP rum med kraftigere computere"}
                           </p>
                         </div>
 
@@ -152,12 +146,7 @@ const Card = forwardRef<HTMLDivElement, CardProps>(
                             size="18"
                           />
                           <p className="text-secondaryCol mt-0">
-
-                            Toast menu{' '}
-                            {variant === 'bday2' && (
-                              <span className="italic font-light">- Inklusiv Dåsesodavand</span>
-                            )}
-
+                            Toast menu {variant === "bday2" && <span className="italic font-light">- Inklusiv Dåsesodavand</span>}
                           </p>
                         </div>
                         <div className="flex flex-row gap-3">
@@ -166,10 +155,7 @@ const Card = forwardRef<HTMLDivElement, CardProps>(
                             size="18"
                           />
 
-                          <p className=" text-secondaryCol mt-0">
-                            En slikpose og en ekstra dåsesodavand.
-                          </p>
-
+                          <p className=" text-secondaryCol mt-0">En slikpose og en ekstra dåsesodavand.</p>
                         </div>
                       </div>
                     ) : (
@@ -189,10 +175,7 @@ const Card = forwardRef<HTMLDivElement, CardProps>(
                             size="18"
                           />
                           <p className=" text-center text-secondaryCol mt-0">
-
-                            {timeAntal && totalPris && Math.round(totalPris / timeAntal)}kr. pr.
-                            time{' '}
-
+                            {timeAntal && totalPris && Math.round(totalPris / timeAntal)}kr. pr. time{" "}
                           </p>
                         </div>
                         <div className="flex flex-row gap-3">
@@ -201,29 +184,21 @@ const Card = forwardRef<HTMLDivElement, CardProps>(
                             size="18"
                           />
                           <p className="max-w-[15ch] text-secondaryCol mt-0">
-
-                            Svarer til {timeAntal ? Math.round((timeAntal * 60) / 34) : 3} spil CS
-                            eller {timeAntal ? Math.round((timeAntal * 60) / 30) : 3} spil PubG
-
+                            Svarer til {timeAntal ? Math.round((timeAntal * 60) / 34) : 3} spil CS eller{" "}
+                            {timeAntal ? Math.round((timeAntal * 60) / 30) : 3} spil PubG
                           </p>
                         </div>
                       </div>
                     )}
 
                     <div>
-
-                      {variant === 'bday1' || variant === 'bday2' ? (
-                        <h3 className="text-secondaryCol text-center text-4xl mt-0">
-                          {totalPris},- pr. pers.
-                        </h3>
+                      {variant === "bday1" || variant === "bday2" ? (
+                        <h3 className="text-secondaryCol text-center text-4xl mt-0">{totalPris},- pr. pers.</h3>
                       ) : (
-                        <h3 className="text-secondaryCol text-center text-4xl mt-0">
-                          {totalPris},-
-                        </h3>
+                        <h3 className="text-secondaryCol text-center text-4xl mt-0">{totalPris},-</h3>
                       )}
 
-                      {!oprettelseInkl && variant !== 'bday1' && variant !== 'bday2' && (
-
+                      {!oprettelseInkl && variant !== "bday1" && variant !== "bday2" && (
                         <p className="text-secondaryCol text-center mt-0">(+35 kr i oprettelse)</p>
                       )}
                     </div>
@@ -235,9 +210,7 @@ const Card = forwardRef<HTMLDivElement, CardProps>(
                   transition={{ duration: 0.1 }}
                   className="flex justify-end -mt-1"
                 >
-
-                  {variant === 'bday1' || variant === 'bday2' ? (
-
+                  {variant === "bday1" || variant === "bday2" ? (
                     <p className="text-secondaryCol uppercase font-bold mt-0">Send forespørgsel</p>
                   ) : (
                     <p className="text-secondaryCol uppercase font-bold mt-0">Book nu</p>
@@ -259,8 +232,6 @@ const Card = forwardRef<HTMLDivElement, CardProps>(
                   className="bg-primaryCol flex justify-between p-7 w-full h-[495px] rounded"
                 >
                   <div className="flex flex-col w-2/5">
-
-
                     <h4 className="text-secondaryCol mt-0">
                       Per time - <span className="text-amber-500">{timePris}kr.</span>
                     </h4>
@@ -331,11 +302,7 @@ const Card = forwardRef<HTMLDivElement, CardProps>(
                               className="flex-shrink-0"
                               size={20}
                             />
-
-                            <p className="text-secondaryCol mt-0">
-                              Logitech G PRO X 2 - LIGHTSPEED
-                            </p>
-´
+                            <p className="text-secondaryCol mt-0">Logitech G PRO X 2 - LIGHTSPEED</p>´
                           </div>
                         </div>
                       </div>
--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,4 +1,5 @@
 import { Layout } from "@/Layout";
+import { Card } from "@/components/Cards/Card";
 import { Hero } from "@/modules/Hero/Hero";
 
 export default function Events() {
@@ -6,15 +7,30 @@ export default function Events() {
     <>
       <Layout>
         <main>
-          <Hero header="Top tier gaming events" redWord={["gaming"]} isFrontPage={false} content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non urna aliquet, mollis lacus sed, dignissim lectus. Curabitur eget diam volutpat, facilisis massa nec, varius nulla." buttonLabel="Kontakt os" />
+          <Hero
+            header="Top tier gaming events"
+            redWord={["gaming"]}
+            isFrontPage={false}
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non urna aliquet, mollis lacus sed, dignissim lectus. Curabitur eget diam volutpat, facilisis massa nec, varius nulla."
+            buttonLabel="Kontakt os"
+          />
           <section>
             <article className="flex justify-center">
               <div className="spacer w-full">
                 <h2>
                   Forskellige typer <span className="text-accentCol">events</span>
                 </h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor euismod posuere. Integer non ullamcorper mi. Vestibulum et congue lorem. Suspendisse iaculis semper mauris eget aliquet. </p>
-                <div className="border-2 border-green-500 ">EventCards goes here!!!!!!!!!!!!!!</div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor euismod posuere. Integer non
+                  ullamcorper mi. Vestibulum et congue lorem. Suspendisse iaculis semper mauris eget aliquet.{" "}
+                </p>
+
+                <div>
+                  <Card
+                    variant={"eventCard"}
+                    header="Børne - fødselsdag"
+                  />
+                </div>
               </div>
             </article>
           </section>
@@ -27,7 +43,13 @@ export default function Events() {
                 <div className="flex flex-col md:flex-row gap-10">
                   <article>
                     <h3>Har du spørgsmål til dit kommende event?</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor euismod posuere. Integer non ullamcorper mi. Vestibulum et congue lorem. Suspendisse iaculis semper mauris eget aliquet. Mauris malesuada purus et rhoncus auctor. Fusce vitae justo risus. Vestibulum convallis justo eu diam dignissim posuere. Donec ac maximus lectus. Praesent non nibh eget sem tincidunt ultricies vel vitae magna. Nullam eget tellus id quam commodo iaculis convallis a odio. Phasellus sagittis velit eget leo. </p>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor euismod posuere. Integer non
+                      ullamcorper mi. Vestibulum et congue lorem. Suspendisse iaculis semper mauris eget aliquet. Mauris malesuada
+                      purus et rhoncus auctor. Fusce vitae justo risus. Vestibulum convallis justo eu diam dignissim posuere.
+                      Donec ac maximus lectus. Praesent non nibh eget sem tincidunt ultricies vel vitae magna. Nullam eget tellus
+                      id quam commodo iaculis convallis a odio. Phasellus sagittis velit eget leo.{" "}
+                    </p>
                   </article>
                   <div className="border-2 border-green-500">Form goes here !!!!!!!!!!</div>
                 </div>
