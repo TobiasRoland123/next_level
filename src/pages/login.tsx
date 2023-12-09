854f74212931c32a726659d15fdc9176de571712
TheFred94 <frederikrlarsen@gmail.com>
2023-12-09 14:48:49 +0100

    animations for mount and errors

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/router";
 import { MdError } from "react-icons/md";
 import { IoIosCheckmarkCircle } from "react-icons/io";
 import * as z from "zod";
+import { AnimatePresence, motion } from "framer-motion";
 import { Layout } from "@/Layout";
 import {
   FormControl,
@@ -89,141 +90,218 @@ export const Login: React.FC<LoginProps> = ({}) => {
   return (
     <>
       <main className="flex justify-center pb-10 ">
-        <section className=" bg-contrastCol/50 backdrop-blur-sm mt-28 mx-4 px-4 py-6 rounded-sm h-fit  ">
-          <h3>Login to access admin page</h3>
-          {isLoginValid === false ? (
-            <h4 className="text-accentCol shake">{supabaseError}</h4>
-          ) : (
-            <h4 className="text-transparent">Placeholder text</h4>
-          )}
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(handleLogin)}
-              className="w-full"
-            >
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem className="mt-5">
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <div
-                        style={{ position: "relative" }}
-                        className={
-                          form.formState.errors.email || isLoginValid === false
-                            ? "shake"
-                            : ""
-                        }
-                      >
-                        <Input
-                          style={{
-                            borderColor:
-                              form.formState.isSubmitted &&
-                              (form.formState.errors.email ||
-                                isLoginValid === false)
-                                ? "red"
-                                : isLoginValid === true
-                                ? "green"
-                                : "none",
-                          }}
-                          {...field}
-                          id="email"
-                          type="email"
-                        />
-                        {form.formState.errors.email ||
-                        isLoginValid === false ? (
-                          <div className="absolute top-2 right-0 pr-3 flex items-center pointer-events-none">
-                            <div>
-                              <MdError className={"text-red-500 text-2xl"} />
-                            </div>
-                          </div>
-                        ) : form.formState.isSubmitted &&
-                          !form.formState.errors.email ? (
-                          <div className="absolute top-2 right-0 pr-3 flex items-center pointer-events-none">
-                            <div>
-                              <IoIosCheckmarkCircle
-                                className={"text-green-500 text-2xl"}
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0, y: "-10%" }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              type: "spring",
+              stiffness: 100,
+              duration: 0.3,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+          >
+            <section className=" bg-contrastCol/50 backdrop-blur-sm mt-28 mx-4 px-4 py-6 rounded-sm h-fit  ">
+              <motion.div
+                initial={{ opacity: 0, y: "-90%" }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  delay: 0.3,
+                  type: "spring",
+                  stiffness: 100,
+                  duration: 0.3,
+                  ease: [0, 0.71, 0.2, 1.01],
+                }}
+              >
+                <h3>Login to access admin page</h3>
+              </motion.div>
+              {isLoginValid === false ? (
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{
+                    type: "spring",
+                    stiffness: 100,
+                    duration: 0.1,
+                    ease: [0, 0.71, 0.2, 1.01],
+                  }}
+                >
+                  <h4 className="text-accentCol shake">{supabaseError}</h4>
+                </motion.div>
+              ) : (
+                <h4 className="text-transparent">Placeholder text</h4>
+              )}
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(handleLogin)}
+                  className="w-full"
+                >
+                  <motion.div
+                    initial={{ opacity: 0, y: "-60%" }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                      delay: 0.3,
+                      type: "spring",
+                      stiffness: 100,
+                      duration: 0.3,
+                      ease: [0, 0.71, 0.2, 1.01],
+                    }}
+                  >
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem className="mt-5">
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <div
+                              style={{ position: "relative" }}
+                              className={
+                                form.formState.errors.email ||
+                                isLoginValid === false
+                                  ? "shake"
+                                  : ""
+                              }
+                            >
+                              <Input
+                                style={{
+                                  borderColor:
+                                    form.formState.isSubmitted &&
+                                    (form.formState.errors.email ||
+                                      isLoginValid === false)
+                                      ? "red"
+                                      : isLoginValid === true
+                                      ? "green"
+                                      : "none",
+                                }}
+                                {...field}
+                                id="email"
+                                type="email"
                               />
+                              {form.formState.errors.email ||
+                              isLoginValid === false ? (
+                                <div className="absolute top-2 right-0 pr-3 flex items-center pointer-events-none">
+                                  <div>
+                                    <MdError
+                                      className={"text-red-500 text-2xl"}
+                                    />
+                                  </div>
+                                </div>
+                              ) : form.formState.isSubmitted &&
+                                !form.formState.errors.email ? (
+                                <div className="absolute top-2 right-0 pr-3 flex items-center pointer-events-none">
+                                  <div>
+                                    <IoIosCheckmarkCircle
+                                      className={"text-green-500 text-2xl"}
+                                    />
+                                  </div>
+                                </div>
+                              ) : (
+                                ""
+                              )}
                             </div>
-                          </div>
-                        ) : (
-                          ""
-                        )}
-                      </div>
-                    </FormControl>
-                    <FormDescription className="text-transparent">
-                      Placeholder text
-                      {/* Remove text-transparent if you need to use the field description */}
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem className="mt-5">
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <div
-                        style={{ position: "relative" }}
-                        className={
-                          form.formState.errors.password ||
-                          isLoginValid === false
-                            ? "shake"
-                            : ""
-                        }
-                      >
-                        <Input
-                          style={{
-                            borderColor:
-                              form.formState.isSubmitted &&
-                              (form.formState.errors.password ||
-                                isLoginValid === false)
-                                ? "red"
-                                : isLoginValid === true
-                                ? "green"
-                                : "none",
-                          }}
-                          {...field}
-                          id="password"
-                          type="password"
-                        />
-                        {form.formState.errors.password ||
-                        isLoginValid === false ? (
-                          <div className="absolute top-2 right-0 pr-3 flex items-center pointer-events-none">
-                            <div>
-                              <MdError className={"text-red-500 text-2xl"} />
-                            </div>
-                          </div>
-                        ) : form.formState.isSubmitted &&
-                          !form.formState.errors.password ? (
-                          <div className="absolute top-2 right-0 pr-3 flex items-center pointer-events-none">
-                            <div>
-                              <IoIosCheckmarkCircle
-                                className={"text-green-500 text-2xl"}
+                          </FormControl>
+                          <FormDescription className="text-transparent">
+                            Placeholder text
+                            {/* Remove text-transparent if you need to use the field description */}
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </motion.div>
+                  <motion.div
+                    initial={{ opacity: 0, y: "-80%" }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                      delay: 0.3,
+                      type: "spring",
+                      stiffness: 100,
+                      duration: 0.3,
+                      ease: [0, 0.71, 0.2, 1.01],
+                    }}
+                  >
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem className="mt-5">
+                          <FormLabel>Password</FormLabel>
+                          <FormControl>
+                            <div
+                              style={{ position: "relative" }}
+                              className={
+                                form.formState.errors.password ||
+                                isLoginValid === false
+                                  ? "shake"
+                                  : ""
+                              }
+                            >
+                              <Input
+                                style={{
+                                  borderColor:
+                                    form.formState.isSubmitted &&
+                                    (form.formState.errors.password ||
+                                      isLoginValid === false)
+                                      ? "red"
+                                      : isLoginValid === true
+                                      ? "green"
+                                      : "none",
+                                }}
+                                {...field}
+                                id="password"
+                                type="password"
                               />
+                              {form.formState.errors.password ||
+                              isLoginValid === false ? (
+                                <div className="absolute top-2 right-0 pr-3 flex items-center pointer-events-none">
+                                  <div>
+                                    <MdError
+                                      className={"text-red-500 text-2xl"}
+                                    />
+                                  </div>
+                                </div>
+                              ) : form.formState.isSubmitted &&
+                                !form.formState.errors.password ? (
+                                <div className="absolute top-2 right-0 pr-3 flex items-center pointer-events-none">
+                                  <div>
+                                    <IoIosCheckmarkCircle
+                                      className={"text-green-500 text-2xl"}
+                                    />
+                                  </div>
+                                </div>
+                              ) : (
+                                ""
+                              )}
                             </div>
-                          </div>
-                        ) : (
-                          ""
-                        )}
-                      </div>
-                    </FormControl>
-                    <FormDescription className="text-transparent">
-                      Placeholder text
-                      {/* Remove text-transparent if you need to use the field description */}
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button>Login</Button>
-            </form>
-          </Form>
-        </section>
+                          </FormControl>
+                          <FormDescription className="text-transparent">
+                            Placeholder text
+                            {/* Remove text-transparent if you need to use the field description */}
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </motion.div>
+                  <motion.div
+                    initial={{ opacity: 0, y: "-100%" }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                      delay: 0.2,
+                      type: "spring",
+                      stiffness: 100,
+                      duration: 0.3,
+                      ease: [0, 0.71, 0.2, 1.01],
+                    }}
+                  >
+                    <Button>Login</Button>
+                  </motion.div>
+                </form>
+              </Form>
+            </section>
+          </motion.div>
+        </AnimatePresence>
       </main>
     </>
   );
