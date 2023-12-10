71fd76c516782b09875e0e8dc80bae58d4342b09
Tobias Roland Uyet <tru@Tobiass-MacBook-Pro.local>
2023-12-10 19:33:57 +0100

    git cleanup after merge

--- src/components/Inputfields/Inputfield.tsx
+++ src/components/Inputfields/Inputfield.tsx
@@ -1,10 +1,9 @@
-import * as React from 'react';
-import { IoIosSearch } from 'react-icons/io';
-import { Label } from './label';
-import { cn } from '../../lib/utils';
-import { useState } from 'react';
+import * as React from "react";
+import { IoIosSearch } from "react-icons/io";
+import { Label } from "./label";
+import { cn } from "../../lib/utils";
+import { useState } from "react";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   labelText?: string;
   isSearch?: boolean;
@@ -22,12 +21,12 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     };
 
     return (
-      <div className='relative'>
+      <div className="relative">
         <Label>{labelText}</Label>
         <input
           type={type}
           className={cn(
-            'flex h-10 w-full rounded bg-contrastCol px-3 py-2 border-b-transparent border-b-2 text-sm file:border-0 transition ease-in duration-300 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-b-2 focus-visible:border-secondaryCol disabled:cursor-not-allowed disabled:opacity-50',
+            "flex h-10 w-full rounded bg-contrastCol px-3 py-2 border-b-transparent border-b-2 text-sm file:border-0 transition ease-in duration-300 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-b-2 focus-visible:border-secondaryCol disabled:cursor-not-allowed disabled:opacity-50",
 
             className
           )}
@@ -39,7 +38,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         {isSearch && isEmpty && inputValue === "" && (
           <div
             className={`absolute h-2 ${
-              labelText ? 'inset-y-10' : 'inset-y-4'
+              labelText ? "inset-y-10" : "inset-y-4"
             } right-0 pr-3 flex items-center pointer-events-none`}
           >
             <div>
@@ -51,6 +50,6 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     );
   }
 );
-Input.displayName = 'Input';
+Input.displayName = "Input";
 
 export { Input };
