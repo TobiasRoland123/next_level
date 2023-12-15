9b56fcdd8260e7bca2fbe3391f7fff7e6aac4a19
Tobias Roland Uyet <tru@Tobiass-MacBook-Pro.local>
2023-12-15 10:58:21 +0100

    when in tablet or above use grid to space evenly

--- src/components/CustomerQuote/CustomerQuote.tsx
+++ src/components/CustomerQuote/CustomerQuote.tsx
@@ -1,3 +1,5 @@
+import { HyphenatedText } from "../HyphenatedText/HyphenatedText";
+
 export interface customerQuoteProps {
   header: string;
   text: string;
@@ -8,8 +10,12 @@ export const CustomerQuote = ({ header, text, author }: customerQuoteProps) => {
   return (
     <>
       <article>
-        <h3>{header}</h3>
-        <p>"{text}"</p>
+        <h3>
+          <HyphenatedText text={header} />
+        </h3>
+        <p>
+          "<HyphenatedText text={text} />"
+        </p>
         <small className="font-bold before:w-6 before:bg-accentCol before:flex before:h-[2px] flex before:my-auto gap-3">
           {author}
         </small>
