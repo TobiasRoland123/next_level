b4a6dc0d89080e3fde7bd4c4d01dc3c1b388b2aa
Tobias Roland <samu1493@stud.kea.dk>
2023-12-17 16:03:11 +0100

    added vercel speed insigts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
-import { Layout } from "@/Layout";
-import "@/styles/globals.css";
-import { Analytics } from "@vercel/analytics/react";
+import { Layout } from '@/Layout';
+import '@/styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     // <Layout>
     <>
       <Component {...pageProps} />
+      <SpeedInsights />
       <Analytics />
     </>
     // </Layout>
